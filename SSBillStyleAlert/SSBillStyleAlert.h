405e7c6ce39a06631fcbb93206b36a25d7311546
Sergey Shinkarenko <sergey.shinkarenko@oxagile.com>
2018-05-23 13:07:20 +0300

    Initial Commit

---
+++ SSBillStyleAlert/SSBillStyleAlert.h
@@ -0,0 +1,19 @@
+//
+//  SSBillStyleAlert.h
+//  SSBillStyleAlert
+//
+//  Created by Sergey Shinkarenko on 5/23/18.
+//  Copyright © 2018 Sergey Shinkarenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSBillStyleAlert.
+FOUNDATION_EXPORT double SSBillStyleAlertVersionNumber;
+
+//! Project version string for SSBillStyleAlert.
+FOUNDATION_EXPORT const unsigned char SSBillStyleAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSBillStyleAlert/PublicHeader.h>
+
+
